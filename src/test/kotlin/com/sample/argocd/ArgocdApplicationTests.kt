c7e5d9d4088b077b6ef148d51db86346699ca0b7
sato-koki <sato.koki@linecorp.com>
2023-10-24 19:03:07 +0900

    first commit

---
+++ src/test/kotlin/com/sample/argocd/ArgocdApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sample.argocd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArgocdApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
