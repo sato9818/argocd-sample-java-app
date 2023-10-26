2ee6095d1a4bb742e97afd2365016657daadf2a0
sato-koki <sato.koki@linecorp.com>
2023-10-26 12:47:21 +0900

    add argocd image updater

--- src/main/kotlin/com/sample/argocd/controller/HelloController.kt
+++ src/main/kotlin/com/sample/argocd/controller/HelloController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
     @GetMapping("/")
     fun sayHello():String {
-        return "Hello World!!"
+        return "Hello World!! part1"
     }
 }
