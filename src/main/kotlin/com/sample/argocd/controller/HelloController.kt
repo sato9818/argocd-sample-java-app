97f2f151ec36c6d22e2c4889f7b3cdda604808ef
sato-koki <sato.koki@linecorp.com>
2023-10-26 19:09:19 +0900

    update

--- src/main/kotlin/com/sample/argocd/controller/HelloController.kt
+++ src/main/kotlin/com/sample/argocd/controller/HelloController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
     @GetMapping("/")
     fun sayHello():String {
-        return "Hello World!! part2"
+        return "Hello World!! 1.0.5 revised"
     }
 }
